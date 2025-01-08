a10bcc59c44fa2d3307f7929c4c8f36495dea4a1
Šimon Drápal <simondrapal9@gmail.com>
2025-01-08 14:55:01 +0100

    připojení k přidávacímu serveru a zisk response + zisk response code a vypsání error + připravená funkce pro send ver. code

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 "use sctrict"
+// informace zda existuje email nebo ne
 document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault()
     let the_email = event.target.elements.email_input_name.value
     if (the_email !== ""){
+        const emailData = { email_data: the_email };
 
+        fetch('https://send-server-field-path-2.onrender.com/control_valid_email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',  // Odesíláme JSON
+            },
+            body: JSON.stringify(emailData),  // Posíláme data jako JSON
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Server response:', data);
+            let response_code = data.response
+            console.log(response_code)
+            if (response_code === 981){
+                error_text("Email allready exists")
+            } else if (response_code === 992){
+                error_text("Invalid email structure")
+            } else if (response_code === 999){
+                send_verify_code(the_email)
+            } else if (response_code === 1002){
+                error_text("There is a mistake")
+            }
+        })
+        .catch(error => console.error('Error:', error));
     } else {
-        document.querySelector("#error_text").style.opacity = 1
+        error_text("Enter your email!")
     }
 })
+
+let error_text = (text) => document.querySelector("#error_text").textContent = text
+
+let send_verify_code = (email) => {
+    console.log("send code!")
+    console.log(email)
+}
