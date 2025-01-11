861ca7564f9bc291b9c8fc0a3482321a24886f72
Šimon Drápal <simondrapal9@gmail.com>
2025-01-11 13:53:23 +0100

    rel img a title + odkazy na sociální sítě v absolute + kontrola validate email tady

--- script.js
+++ script.js
@@ -4,32 +4,36 @@ document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault()
     let the_email = event.target.elements.email_input_name.value
     if (the_email !== ""){
-        const emailData = { email_data: the_email };
+        const emailRex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if (emailRex.test(the_email)){
+            const emailData = { email_data: the_email };
 
-        fetch('https://send-server-field-path-2.onrender.com/control_valid_email', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',  // Odesíláme JSON
-            },
-            body: JSON.stringify(emailData),  // Posíláme data jako JSON
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Server response:', data);
-            let response_code = data.response
-            console.log(response_code)
-            if (response_code === 981){
-                error_text("Email allready exists")
-            } else if (response_code === 992){
-                error_text("Invalid email structure")
-            } else if (response_code === 999){
-                // send_verify_code(the_email)
-                window.location.href = "/verify_code/verify_code.html?email=" + encodeURIComponent(the_email)
-            } else if (response_code === 1002){
-                error_text("There is a mistake")
-            }
-        })
-        .catch(error => console.error('Error:', error));
+            fetch('https://send-server-field-path-2.onrender.com/control_valid_email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',  // Odesíláme JSON
+                },
+                body: JSON.stringify(emailData),  // Posíláme data jako JSON
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Server response:', data);
+                let response_code = data.response
+                console.log(response_code)
+                if (response_code === 981){
+                    error_text("Email allready exists")
+                } else if (response_code === 999){
+                    // send_verify_code(the_email)
+                    window.location.href = "/verify_code/verify_code.html?email=" + encodeURIComponent(the_email)
+                } else if (response_code === 1002){
+                    error_text("There is a mistake")
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        } else {
+            error_text("Invalid email structure")
+            console.log("here")
+        }
     } else {
         error_text("Enter your email!")
     }
