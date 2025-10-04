b30e44ba885fe021a67d96638f115adb904f3921
Šimon Drápal <simondrapal9@gmail.com>
2025-10-04 10:54:10 +0200

    smazání delete account + odkaz na google play a app store + základní překlad stránky en a cs

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 "use sctrict"
 // menu //
 
+function setLanguage(lang) {
+    document.querySelectorAll('[data-cs]').forEach(el => {
+      el.textContent = el.dataset[lang];
+    });
+    document.documentElement.lang = lang === 'cs' ? 'cs' : 'en';
+    localStorage.setItem('lang', lang); // uloží volbu uživatele
+  }
+
+fetch('https://ipwhois.app/json/')
+  .then(res => res.json())
+  .then(data => {
+    const country = data.country_code;
+    if(["CZ", "SK"].includes(country)) setLanguage('cs');
+    else setLanguage('en');
+  })
+  .catch(err => console.error(err));
+
 document.querySelector("#menu_button").addEventListener("click", (event) => {
     event.preventDefault();
 
